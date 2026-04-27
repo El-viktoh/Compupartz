42d66fc9cd0cd6b29b6e75f53d7f8301ebb2679d
Victor Yeboah <victoryeboah@Victors-MacBook-Pro.local>
2026-04-27 08:07:30 +0000

    Fix missing imports in store views

--- store/views.py
+++ store/views.py
@@ -1,3 +1,5 @@
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.decorators import login_required
 from django.http import JsonResponse
 from .models import Product, Review, Wishlist
 
